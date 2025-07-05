4527dd7f937e4a68e9411c7bd1ba8a40a23a98da
Nadya Ershova <nadezhda18@gmail.com>
2025-07-04 21:35:19 -0400

    Added TODOs

--- invoice_proof.py
+++ invoice_proof.py
@@ -20,6 +20,9 @@ def callback(message):
 
 def add_invoices_to_tasks(invoice_title, start, end):
 
+    #TODO: ADD PAGINATION SUPPORT TO TASK pagination - or add a filter to show only tasks w/o invoices and run it multiple times!!!
+    #TODO: Add '# of items' equals 1 in the invoice creation
+
     #TODO: Eventually, add a param for the person the task is assigned to.
 
     person = VA_ASMAT_ID  # Assuming VA_ASMAT_ID is the ID of the person to whom tasks are assigned
